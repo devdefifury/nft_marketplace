1b0c1c6cacc7bb328f937b8e90b09a6bd2d9c99f
creativetemmy <temitopeayo01@gmail.com>
2023-10-06 12:41:48 +0100

    added a utility function

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import { Banner } from '../components'
+import { useStat, useEffect, useRef } from 'react'
+
+import { Banner, CreatorCard } from '../components'
+import images from '../assets'
+import { makeId } from '../utils/makeId'
 
 const Home = () => {
+  const parentRef = useRef(null)
+  const scrollRef = useRef(null)
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -9,6 +16,29 @@ const Home = () => {
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
           parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
         />
+
+        <div>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml0">
+            Best Creator
+          </h1>
+
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+            <div
+              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+              ref={scrollRef}
+            >
+              {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  creatorImage={images[`creator${i}`]}
+                  creatorName={`0x${makeId(3)}...${makeId(4)}`}
+                  creatorEths={10 - i * 0.5}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
